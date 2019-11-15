c5fe80f62e69bafbec55df153812144a4d198953
Sachi-A <sachin.kumar1_cs17@gla.ac.in>
2019-11-16 00:01:29 +0530

    testing

---
+++ venv/bin/Question.py
@@ -0,0 +1,14 @@
+import random
+import itertools
+
+results = {
+    'h': 0,
+    't': 0,
+}
+sides = list(results.keys())
+n = int(input())
+for i in range(n):
+    results[random.choice(sides)] += 1
+
+print('Heads:', results['h'])
+print('Tails:', results['t'])
